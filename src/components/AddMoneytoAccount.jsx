385538a974cc77701f5a2f698712743f2ac02a70
Lrev47 <luis.revilla47@yahoo.com>
2024-03-20 19:48:21 -0500

    add money to account almost up

--- src/components/AddMoneytoAccount.jsx
+++ src/components/AddMoneytoAccount.jsx
@@ -1,7 +1,61 @@
 import { useState } from "react";
+import { useUpdateUserMoneyMutation } from "../StoreApi";
+import { useParams } from "react-router-dom";
 
-const AddMoneyToAccount = () => {
-  return <div></div>;
+const AddMoneyToAccount = ({ token, onMoneyAdded }) => {
+  const [amount, setAmount] = useState("");
+  const { userId } = useParams();
+  const [updateUserMoney] = useUpdateUserMoneyMutation();
+
+  const updateAmount = (e) => {
+    const newAmount = parseInt(e.target.value, 10);
+
+    if (!isNaN(newAmount) && newAmount >= 0) {
+      setAmount(newAmount);
+    } else if (e.target.value === "") {
+      setAmount("");
+    }
+  };
+
+  const submitMoneytoAccount = async (newAmount, userId) => {
+    try {
+      await updateUserMoney({
+        userId,
+        newMoneyAmount: newAmount,
+        token,
+      }).unwrap();
+      console.log("Money Updated");
+      setAmount("");
+      onMoneyAdded();
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const onSubmit = () => {
+    if (amount >= 0) {
+      submitMoneytoAccount(amount, userId);
+    } else {
+      console.log("no negative value");
+    }
+  };
+
+  return (
+    <div className="AddMoneyToAccountContainer">
+      <h3>Add Money to your Account</h3>
+      <div className="MoneyInputContainer">
+        <input
+          className="MoneyInput"
+          type="Number"
+          value={amount}
+          onChange={updateAmount}
+        ></input>
+      </div>
+      <button className="AddMoneyButton" onClick={onSubmit}>
+        Add Money
+      </button>
+    </div>
+  );
 };
 
 export default AddMoneyToAccount;
