a7c6994ce80c3dc4692a0078ed08c86a93d225ca
luis revilla <lrev4766@gmail.com>
2024-12-26 20:13:17 -0600

    added pageanation

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -9,12 +9,18 @@ const HomePage = () => {
   const navigate = useNavigate();
   const { data, error, isLoading } = useGetAllProductsQuery();
   const [productId, setProductId] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 20;
 
   const handleClick = (productId) => {
     navigate(`/products/${productId}`);
     window.scrollTo({ top: 0, behavior: "smooth" });
     setProductId(productId);
-    // console.log("You Clicked Product Number ", productId);
+  };
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   if (isLoading) {
@@ -23,7 +29,13 @@ const HomePage = () => {
   if (error) {
     return <div>{error.message}</div>;
   }
-  // console.log(data);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentItems = data ? data.slice(startIndex, endIndex) : [];
+
+  const totalPages = data ? Math.ceil(data.length / itemsPerPage) : 0;
+
   return (
     <>
       <div>
@@ -31,24 +43,37 @@ const HomePage = () => {
       </div>
 
       <div className="HomePageProductsContainer">
-        {data &&
-          data.map((product) => (
-            <div
-              onClick={() => handleClick(product.id)}
-              className="ProductContainer"
-              key={product.id}
-            >
-              <img
-                className="HomePageProductImage"
-                src={product.imageUrl}
-                alt={product.name}
-              />
-              <div className="ImageOverlay">{product.name}</div>
-            </div>
-          ))}
+        {currentItems.map((product) => (
+          <div
+            onClick={() => handleClick(product.id)}
+            className="ProductContainer"
+            key={product.id}
+          >
+            <img
+              className="HomePageProductImage"
+              src={product.imageUrl}
+              alt={product.name}
+            />
+            <div className="ImageOverlay">{product.name}</div>
+          </div>
+        ))}
         {productId && <SingleProduct productId={productId} />}
+      </div>
+
+      <div className="Pagination">
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button
+            className="PageButton"
+            key={index}
+            onClick={() => handlePageChange(index + 1)}
+            disabled={currentPage === index + 1}
+          >
+            {index + 1}
+          </button>
+        ))}
       </div>
     </>
   );
 };
+
 export default HomePage;
